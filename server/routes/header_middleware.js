247637b90eb48cce5615e2640e84568a0f949536
Sylvain Telkostrasz Cossement <telkostrasz@totalrp3.info>
2017-10-17 22:56:09 +0200

    calcul couleur perso

--- server/routes/header_middleware.js
+++ server/routes/header_middleware.js
@@ -42,35 +42,48 @@ exports.header = function (req, res, next) {
 			],
 			startDate: {$gt: new Date()}
 		};
-		
+
 		queries.push(CalendarEntry.model.count(query).exec().then((count) => {
 			user.event_count = count;
 		}));
 	}
-	
-	// Chargement des groupes && droits
+
+	// Chargement des groupes && droits + couleur principale du user
 	{
 		queries.push(User.model.findOne({_id: user._id}).select("permissions.groups").populate("permissions.groups").exec().then((user) => {
+			user.permissions.groups.sort((a, b) => {
+				if (a.order < b.order)
+					return -1;
+				if (a.order > b.order)
+					return 1;
+				return 0;
+			});
+
 			locals.user.permissions.groups = user.permissions.groups;
 			// TODO: inject group global rights
 			res.locals.rightKeysSet = new Set();
-			
+
 			// Au moins un groupe BN, donc le gars est BN
 			let isBN = false;
+			let color;
 			user.permissions.groups.forEach(g => {
-				if(g.isBN){
+				if (g.isBN) {
+					if (!color)
+						color = g.color;
 					isBN = true;
 				}
 			});
 			locals.user.isBN = isBN;
+			locals.user.color = color;
 		}));
 	}
 
 	locals.discord_users = discord.getOnlineUsers();
 	user.discord_count = locals.discord_users.length;
-	
+
 	// Maj async de la présence
-	User.model.update({_id: user._id}, {connectDate: new Date()}, err => {});
+	User.model.update({_id: user._id}, {connectDate: new Date()}, err => {
+	});
 
 	Promise.all(queries).then(() => {
 		next();
