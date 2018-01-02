d667b35a426180e442d4bec206bdad6e185f359e
Sylvain Telkostrasz Cossement <telkostrasz@totalrp3.info>
2018-01-02 17:36:40 +0100

    Polish markdown editor

--- server/templates/views/widget/markdown_editor.jsx
+++ server/templates/views/widget/markdown_editor.jsx
@@ -1,5 +1,11 @@
 import * as FetchUtils from "../../../../public/js/utils/FetchUtils.jsx";
 
+const editorInsertText = (textarea, txtToAdd) => {
+	const caretPos = textarea[0].selectionStart;
+	const textAreaTxt = textarea.val();
+	textarea.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+};
+
 export default {
 
 	config: function (idPrefix) {
@@ -7,11 +13,29 @@ export default {
 		const preview = $(`#${idPrefix}-preview`);
 		const previewButton = $(`#${idPrefix}-preview-b`);
 
+		const wrapper = textarea.parent();
+		
+		const yt = wrapper.find(".tool-yt");
+		yt.click(() => {
+			editorInsertText(textarea, "YT[id_de_la_video]");
+		});
+		yt.tooltip({
+			title: "Insérer une balise de vidéo Youtube"
+		});
+		
+		const straw = wrapper.find(".tool-straw");
+		straw.click(() => {
+			editorInsertText(textarea, "POLL[id_du_strawpoll]");
+		});
+		straw.tooltip({
+			title: "Insérer une balise de sondage Strawpoll"
+		});
+
 		preview.hide();
-		textarea.show();
+		wrapper.show();
 		previewButton.click(() => {
 			preview.toggle();
-			textarea.toggle();
+			wrapper.toggle();
 			if (preview.is(":visible")) {
 				preview.text("Chargement de l'aperçu...");
 				FetchUtils.post('post', 'preview', {raw: textarea.val()}, {
