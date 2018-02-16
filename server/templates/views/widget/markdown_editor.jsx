62b2b5d8595dbbdec74260f682169065901d5455
Sylvain Telkostrasz Cossement <telkostrasz@totalrp3.info>
2018-02-16 21:37:54 +0100

    Better editor

--- server/templates/views/widget/markdown_editor.jsx
+++ server/templates/views/widget/markdown_editor.jsx
@@ -5,11 +5,47 @@ let giphy = require('giphy-api')({
 	https: true
 });
 
-const editorInsertText = (textarea, txtToAdd) => {
+function setSelectionRange(input, selectionStart, selectionEnd) {
+	if (input.setSelectionRange) {
+		input.focus();
+		input.setSelectionRange(selectionStart, selectionEnd);
+	} else if (input.createTextRange) {
+		const range = input.createTextRange();
+		range.collapse(true);
+		range.moveEnd('character', selectionEnd);
+		range.moveStart('character', selectionStart);
+		range.select();
+	}
+}
+
+const editorInsertText = (textarea, txtToAdd, wrapStartPos, wrapEndPos) => {
+	wrapStartPos = wrapStartPos || txtToAdd.length;
+	wrapEndPos = wrapEndPos || txtToAdd.length;
 	const caretPos = textarea[0].selectionStart;
 	const textAreaTxt = textarea.val();
 	textarea.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos));
-	textarea.focus();
+	setSelectionRange(textarea[0], caretPos + wrapStartPos, caretPos + wrapEndPos);
+};
+
+const editorWrapText = (textarea, textStart, textEnd, wrapStartPos, wrapEndPos) => {
+	textEnd = textEnd || textStart;
+	const caretPos = textarea[0].selectionStart;
+	const caretPosEnd = textarea[0].selectionEnd;
+	const textAreaTxt = textarea.val();
+
+	if (caretPos < caretPosEnd) {
+		textarea.val(textAreaTxt.substring(0, caretPos) + textStart + textAreaTxt.substring(caretPos, caretPosEnd) + textEnd + textAreaTxt.substring(caretPosEnd));
+
+		if(wrapStartPos && wrapEndPos) {
+			setSelectionRange(textarea[0], caretPos + wrapStartPos, caretPos + wrapEndPos);
+		} else {
+			setSelectionRange(textarea[0], caretPosEnd + (textStart.length + textEnd.length), caretPosEnd + (textStart.length + textEnd.length));
+		}
+	} else {
+		textarea.val(textAreaTxt.substring(0, caretPos) + textStart + textEnd + textAreaTxt.substring(caretPos));
+		setSelectionRange(textarea[0], caretPos + textStart.length, caretPos + textStart.length);
+	}
+
 };
 
 export default {
@@ -21,22 +57,79 @@ export default {
 
 		const wrapper = textarea.parent();
 
+		// Youtube
 		const yt = wrapper.find(".tool-yt");
 		yt.click(e => {
-			editorInsertText(textarea, "YT[id_de_la_video]");
+			editorInsertText(textarea, "YT[id_de_la_video]", 3, 17);
 		});
 		yt.tooltip({
 			title: "Insérer une balise de vidéo Youtube"
 		});
 
+		// Strawpoll
 		const straw = wrapper.find(".tool-straw");
 		straw.click(() => {
-			editorInsertText(textarea, "POLL[id_du_strawpoll]");
+			editorInsertText(textarea, "POLL[id_du_strawpoll]", 5, 20);
 		});
 		straw.tooltip({
 			title: "Insérer une balise de sondage Strawpoll"
 		});
 
+		// Bold
+		const bold = wrapper.find(".tool-b");
+		bold.click(() => {
+			editorWrapText(textarea, "**");
+		});
+		bold.tooltip({
+			title: "Mettre en gras"
+		});
+
+		// Italic
+		const italic = wrapper.find(".tool-i");
+		italic.click(() => {
+			editorWrapText(textarea, "*");
+		});
+		italic.tooltip({
+			title: "Mettre en souligné"
+		});
+
+		// Underline
+		const underline = wrapper.find(".tool-u");
+		underline.click(() => {
+			editorWrapText(textarea, "<u>", "</u>");
+		});
+		underline.tooltip({
+			title: "Mettre en italic"
+		});
+
+		// Center
+		const center = wrapper.find(".tool-center");
+		center.click(() => {
+			editorWrapText(textarea, "<center>", "</center>");
+		});
+		center.tooltip({
+			title: "Centrer le texte"
+		});
+
+		// Link
+		const link = wrapper.find(".tool-link");
+		link.click(() => {
+			editorWrapText(textarea, "[Texte du lien](", ")", 1, 14);
+		});
+		link.tooltip({
+			title: "Insérer un lien"
+		});
+
+		// Image
+		const image = wrapper.find(".tool-image");
+		image.click(() => {
+			editorWrapText(textarea, "![](", ")");
+		});
+		image.tooltip({
+			title: "Insérer un lien d'image"
+		});
+
+		// Emoji
 		wrapper.find(".emoji").click(e => {
 			editorInsertText(textarea, $(e.target).text());
 		});
@@ -85,7 +178,7 @@ export default {
 						if (res.data.length) {
 							gifsSearchModalContainer.empty();
 							res.data.forEach(gif => {
-								gifsSearchModalContainer.append( `<img class="gif-preview" src='https://i.giphy.com/media/${gif.id}/giphy-preview.gif' gif="${gif.id}"/>` );
+								gifsSearchModalContainer.append(`<img class="gif-preview" src='https://i.giphy.com/media/${gif.id}/giphy-preview.gif' gif="${gif.id}"/>`);
 							});
 							gifsSearchModal.modal('show');
 							$("img.gif-preview").click(e => {
