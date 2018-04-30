1486277d082344990966ad4b34bb426ced040bc2
Cossement Sylvain <cossement.sylvain@gmail.com>
2018-04-30 21:54:21 +0200

    fix table replacement width

--- server/config.js
+++ server/config.js
@@ -45,7 +45,7 @@ showdown.extension('tableExt', function () {
 	return [{
 		type: "output",
 		filter: function (html, converter, options) {
-			return html.replace(/<table>/g, `<table class='table table-striped table-bordered' style='table-layout: fixed;'>`);
+			return html.replace(/<table>/g, `<table class='table table-striped table-bordered'>`);
 		}
 	}];
 });
